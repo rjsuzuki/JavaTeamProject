338e7fee834c5e0e4a21dbf21d9920b6e57a39c6
Ryan Suzuki <ryanjsuzuki@gmail.com>
2015-06-07 14:08:02 +0900

    Updates

--- RegistrationSystem.java
+++ RegistrationSystem.java
@@ -3,23 +3,21 @@ import java.text.SimpleDateFormat;
 import java.io.*;
 import java.util.*;
 
-public class RegistrationSystem {
+public class RegistrationSystem implements Serializable {
+
     //For User Input
-    File schoolRegistrar;
-    Scanner fileScanner;
-    Scanner userInputScanner;
-    PrintWriter saveToFile;
+    private File studentRegistryList;
+    private Scanner fileScanner;
+    private Scanner userInputScanner;
+    ArrayList<Student> studentList = new ArrayList<Student>();
 
-    String firstName;
-    String lastName;
-    String userLogin;
-
-    ArrayList<Student> studentList;
+    private int maximumStudentCapacity = 30;
     private int courseMaxSize = 10;
     private int courseCurrentSize = 0;
-    private CourseList courseList;
 
     public static void main(String[] args) {
+
+
         RegistrationSystem builder = new RegistrationSystem();
         builder.go();
 
@@ -27,7 +25,7 @@ public class RegistrationSystem {
 
     public void go() {
 
-        //Student Registration Begins here, first and last name.
+        //Student Registration Begins here
         try {
           //Set up Scanner object using system.in
           userInputScanner = new Scanner(System.in);
@@ -36,27 +34,16 @@ public class RegistrationSystem {
           System.out.println("Welcome to the University of Irvine Online Registration System!");
           System.out.println("To create a new user press 1. To login press 2.");
           String menuOption = userInputScanner.next();
-          int number = Integer.parseInt(menuOption);
+          int number = Integer.parseInt(menuOption); //Converts user input to an integer
           if (number == 1) {
             userRegistration();
           } else if (number == 2) {
-            userLogin();
+            courseRegistration();
           } else {
             System.out.println("Invalid input. Please choose again.");
-            String userInput = userInputScanner.next();
+            go();
           }
 
-          //After registration, load new screen to enter login include
-
-          //check database for user, then load menu
-
-          //menu selection - show available courses or view registered courses.
-
-          //available courses menu - show list with info - also give option to register for course
-
-          //registered courses menu - show list with info - also give option to un-register a course.
-
-          saveToFile.close();
           userInputScanner.close();
         } catch (Exception e) {
           e.printStackTrace();
@@ -76,29 +63,37 @@ public class RegistrationSystem {
           System.out.println("Please enter your last name: ");
           String userLastName = userInputScanner.next();
 
+          System.out.println("Please enter your age: ");
+          int userAge = userInputScanner.nextInt();
+
+          System.out.println("Please enter your gender (M/F): ");
+          char userGender = userInputScanner.next().charAt(0);
+
           System.out.println("Please enter your SSN: ");
-          String ssn = userInputScanner.next();
-          int foo = Integer.parseInt(ssn);
+          String userSsn = userInputScanner.next();
 
-          System.out.println("Thank you " + userFirstName + " " + userLastName);
+          System.out.println("Please enter a 5-digit student ID number:");
+          String studentId = userInputScanner.next();
 
-          System.out.println("Please enter your login name: ");
-          String userLogin = userInputScanner.next();
-
-          //Instantiate a new User with User's input
-          Student student = new Student (userFirstName, userLastName, foo);
-
-          //add new User to courseList
-          studentList.add(student);
+          //Create a new User with User's input and add to studentList
+          studentList.add(new Student(userFirstName, userLastName, userAge, userGender, userSsn, studentId));
 
           //save user's input to file
-          File schoolRegistrar = new File("database.txt");
-          fileScanner = new Scanner(schoolRegistrar);
+          FileOutputStream fileOut = new FileOutputStream("studentRegistryList.ser"); // creates a serial file in ouput stream
+          ObjectOutputStream out = new ObjectOutputStream(fileOut); // routes an object into the output stream
+            out.writeObject(studentList); // write specified object(s) into file
+          out.close();
+          fileOut.close();
+
 
           //validate if ssn = unique?
 
-          //Return screen to login page
+          //use equals() to compare strings
+          System.out.println("Thank you " + userFirstName + " " + userLastName);
+          System.out.println("You have now been added to the database.");
 
+          //Return screen to login page
+          go();
 
           userInputScanner.close();
         } catch (Exception e) {
@@ -107,117 +102,137 @@ public class RegistrationSystem {
     }
 
     //*********************************To sign in a registered User*********************************
-    public void userLogin() {
+    /*public void userLogin() {
 
       try {
           userInputScanner = new Scanner(System.in);
           //Prompt User for login name
-          System.out.println("Please enter your login name: ");
+          System.out.println("Please enter your student ID: ");
           String login = userInputScanner.next();
 
           //open file and retrieve user info from database
+          try {
+              FileInputStream fileIn = new FileInputStream("studentRegistryList.ser");
+              ObjectInputStream in = new ObjectInputStream(fileIn);
+              deserializedStudentList = (ArrayList)in.readObject();
+              in.close();
+              fileIn.close();
 
+          } catch (IOException i) {
+              i.printStackTrace();
+          } catch (ClassNotFoundException e) {
+              e.printStackTrace();
+          }
 
-          //then
+          //Loop through array and find matching ID, if false,
+          for (Student student : deserializedStudentList) {
+              if (student.getStudentId().equals(login)) {
+                  System.out.println("Successful login.");
+                  courseRegistration();
+              } else {
+                    System.out.println("User not found. Please re-enter student ID:");
+                    //userLogin();
+              }
+          }
           userInputScanner.close();
       } catch (Exception e) {
             e.printStackTrace();
       }
 
-    }
+    }*/
 
     //*********************************Course registration option*********************************
     public void courseRegistration() {
 
-        try {
-          userInputScanner = new Scanner(System.in);
-          System.out.println("Please choose from the following options");
-          System.out.println("To view available courses, press 1.");
-          System.out.println("To view registered courses, press 2.");
-          System.out.println("To register for course, press 3.");
-          System.out.println("To unregister for course, press 4.");
-          String courseOption = userInputScanner.next();
-          int number = Integer.parseInt(courseOption);
+            try {
+              userInputScanner = new Scanner(System.in);
+              System.out.println("Please choose from the following options");
+              System.out.println("To view available courses, press 1.");
+              System.out.println("To view registered courses, press 2.");
+              System.out.println("To register for course, press 3.");
+              System.out.println("To unregister for course, press 4.");
+              String courseOption = userInputScanner.next();
+              int number = Integer.parseInt(courseOption);
 
-          if (number == 1) {
-              //Show available courses + option to register for course.
-              // NOTE: which method is wanted here?
-              //courseList.viewCourseList();  // displays all courses
-              courseList.viewAvailableCourseList(); // displays courses w/ seats available
-          } else if (number == 2) {
-              //Show registered courses + option to un-register from course.
-          } else if (number == 3) {
-              System.out.println("Enter course id");
-              int courseId = Integer.parseInt(userInputScanner.next());
-              // NOTE: should be ssn or student id
-              System.out.println("Enter Social Security Number");
-              int ssn = Integer.parseInt(courseOption);
-              // Get student
-              Student student = null;
-              for(Student s: studentList) {
-                  if(s.getStudentSsn() == ssn) {
-                      student = s;
+              if (number == 1) {
+                  //Show available courses + option to register for course.
+                  // NOTE: which method is wanted here?
+                  //courseList.viewCourseList();  // displays all courses
+                  courseList.viewAvailableCourseList(); // displays courses w/ seats available
+              } else if (number == 2) {
+                  //Show registered courses + option to un-register from course.
+              } else if (number == 3) {
+                  System.out.println("Enter course id");
+                  int courseId = Integer.parseInt(userInputScanner.next());
+                  // NOTE: should be ssn or student id
+                  System.out.println("Enter Social Security Number");
+                  int ssn = Integer.parseInt(courseOption);
+                  // Get student
+                  Student student = null;
+                  for(Student s: studentList) {
+                      if(s.getStudentSsn() == ssn) {
+                          student = s;
+                      }
                   }
-              }
-              if(student != null) {
-                  // Register for course
-                  boolean enrollmentStatus = courseList.registerForCourse(courseId, student);
-                  if(enrollmentStatus == false) {
-                      System.out.println("Student course registration failed");
+                  if(student != null) {
+                      // Register for course
+                      boolean enrollmentStatus = courseList.registerForCourse(courseId, student);
+                      if(enrollmentStatus == false) {
+                          System.out.println("Student course registration failed");
+                      } else {
+                          System.out.println("Registered for course");
+                          // TODO: add course list to Student
+                          //student.addCourse(id);
+                      }
                   } else {
-                      System.out.println("Registered for course");
-                      // TODO: add course list to Student
-                      //student.addCourse(id);
+                      // Failed to find student in studentList
+                      System.out.println("Could not find student enrolled.");
+                  }
+              } else if (number == 4) {
+                  System.out.println("Enter course id");
+                  int courseId = Integer.parseInt(userInputScanner.next());
+                  // NOTE: should be ssn or student id
+                  System.out.println("Enter Social Security Number");
+                  int ssn = Integer.parseInt(courseOption);
+                  // Get student from social security number
+                  Student student = null;
+                  for(Student s: studentList) {
+                      if(s.getStudentSsn() == ssn) {
+                          student = s;
+                      }
+                  }
+                  if(student != null) {
+                      // Register for course
+                      boolean enrollmentStatus = courseList.unregisterForCourse(courseId, student);
+                      if(enrollmentStatus == false) {
+                          System.out.println("Course unregistration failed");
+                      } else {
+                          System.out.println("Unregistered for course");
+                          // TODO: add course list to Student
+                          //student.removeCourse(id);
+                      }
+                  } else {
+                      // Failed to find student in studentList
+                      System.out.println("Could not find student enrolled.");
                   }
               } else {
-                  // Failed to find student in studentList
-                  System.out.println("Could not find student enrolled.");
+                  //Return invalid input message and try again.
               }
-          } else if (number == 4) {
-              System.out.println("Enter course id");
-              int courseId = Integer.parseInt(userInputScanner.next());
-              // NOTE: should be ssn or student id
-              System.out.println("Enter Social Security Number");
-              int ssn = Integer.parseInt(courseOption);
-              // Get student from social security number
-              Student student = null;
-              for(Student s: studentList) {
-                  if(s.getStudentSsn() == ssn) {
-                      student = s;
-                  }
-              }
-              if(student != null) {
-                  // Register for course
-                  boolean enrollmentStatus = courseList.unregisterForCourse(courseId, student);
-                  if(enrollmentStatus == false) {
-                      System.out.println("Course unregistration failed");
-                  } else {
-                      System.out.println("Unregistered for course");
-                      // TODO: add course list to Student
-                      //student.removeCourse(id);
-                  }
-              } else {
-                  // Failed to find student in studentList
-                  System.out.println("Could not find student enrolled.");
-              }
-          } else {
-              //Return invalid input message and try again.
-          }
 
-          userInputScanner.close();
-        } catch (Exception e) {
-              e.printStackTrace();
+              userInputScanner.close();
+            } catch (Exception e) {
+                  e.printStackTrace();
+            }
+
         }
 
-    }
+        //Changing course size
+        public int getCourseSize() {
+            return courseCurrentSize;
+        }
 
-    //Changing course size
-    public int getCourseSize() {
-        return courseCurrentSize;
-    }
-
-    RegistrationSystem() {
-        courseList = new CourseList();
-    }
+        RegistrationSystem() {
+            courseList = new CourseList();
+        }
 
 } //end of Class
