9ef3374be29ef63f565086eb660dfc05b9f1d753
Ryan Suzuki <ryanjsuzuki@gmail.com>
2015-06-07 15:41:35 +0900

    No actual changes occur

--- CourseList.java
+++ CourseList.java
@@ -24,7 +24,7 @@ public class CourseList {
 	{
 		// Sort the peopleList ArrayList by last name in alphabetical order
 		Collections.sort(courseList, Course.CourseNameComparator);
-		
+
 		//Iterate over courseList, view each course info
 		for(Course c: courseList) {
 			c.viewCourseInfo();
@@ -35,7 +35,7 @@ public class CourseList {
 	{
 		// Sort the peopleList ArrayList by last name in alphabetical order
 		Collections.sort(courseList, Course.CourseNameComparator);
-		
+
 		//Iterate over courseList, view each course info
 		for(Course c: courseList) {
 			if(c.isSeatAvailable()) {
@@ -53,21 +53,21 @@ public class CourseList {
 	}
 	public boolean registerForCourse(int id, Student student) {
 		Course course = getCourse(id);
-		return course.registerStudent(student);		
+		return course.register(student);
 	}
 	public boolean unregisterForCourse(int id, Student student) {
 		Course course = getCourse(id);
-		return course.unregisterStudent(student);		
+		return course.unregister(student);
 	}
 	public void getCourseList() {
     	File courseListFile;
 		Scanner fileScanner;
- 		courseList = new ArrayList<Course>();		
+ 		courseList = new ArrayList<Course>();
 		try
 		{
 			// Open file CourseList.txt file
 			courseListFile = new File("courseList.txt");
-			
+
 			fileScanner = new Scanner(courseListFile);
 			// Set delimeter ',' and newline
 			fileScanner.useDelimiter("[,\\n]");
@@ -78,7 +78,7 @@ public class CourseList {
 			int i=0;
 			while(fileScanner.hasNext()) {
 				// Create Course instance w/ info from input file
-				course = new Course();		
+				course = new Course();
 				course.setCourseId(Integer.parseInt(fileScanner.next())); // Course Id
 				course.setCourseName(fileScanner.next()); // Name
 				course.setSummary(fileScanner.next());
@@ -86,16 +86,17 @@ public class CourseList {
 				int month = Integer.parseInt(fileScanner.next());
 				int day = Integer.parseInt(fileScanner.next());
 				int year = Integer.parseInt(fileScanner.next());
-				Date startDate = new Date(month, day, year);
+
 				month = Integer.parseInt(fileScanner.next());
 				day = Integer.parseInt(fileScanner.next());
 				year = Integer.parseInt(fileScanner.next());
-				Date endDate = new Date(month, day, year);
+				Datex startDate = new Datex(month, day, year);
+				Datex endDate = new Datex(month, day, year);
 				course.setDate(startDate, endDate);
 				courseList.add(course);
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}	
-    }   	
+		}
+    }
 }
