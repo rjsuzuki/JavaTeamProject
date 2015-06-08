b291f0ab0167d36709cdff579bf1d64707a18373
Joan Siebenaler <jms80304@gmail.com>
2015-06-08 01:09:01 -0600

    Add register and unregister course.

--- Student.java
+++ Student.java
@@ -4,11 +4,13 @@ import java.io.*;
 public class Student extends Person implements Serializable {
 
   private String studentId;
+  private ArrayList<Course> courseList;
 
   //Constructor
   public Student(String firstName, String lastName, int age, char gender, String ssn, String studentId) {
       super(firstName, lastName, age, gender, ssn);
       this.studentId = studentId;
+      this.courseList = new ArrayList<Course>();
   }
 
   public void setStudentId(String studentId) {
@@ -23,5 +25,18 @@ public class Student extends Person implements Serializable {
       return super.getSsn();
   }
 
-
+  public void registerForCourse(Course course) {
+	  courseList.add(course);
+  }
+  
+  public void unregisterForCourse(Course course) {
+	  courseList.remove(course);
+  }
+  
+  public void viewCourseList() {
+		//Iterate over courseList, view each course info
+		for(Course c: courseList) {
+			c.viewCourseInfo();
+		}
+  }
 } //end of Class
